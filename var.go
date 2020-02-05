ae4be8b32eea0314e044955ec417ae65f890b21a
hota1024 <hotalog@hota1024.com>
2020-02-05 13:30:42 +0900

    Add var.go

---
+++ var.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var num int = 10
+	var added = num + 10
+	var str = "Hello"
+	a := 10
+
+	fmt.Println(num)
+	fmt.Println(added)
+	fmt.Println(str)
+	fmt.Println(a)
+}
